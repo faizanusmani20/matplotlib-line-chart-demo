b199d41f6305b0b0baa324fab0068f4c9deddd24
benignUmbra <faizanusmani020@gmail.com>
2026-06-05 00:40:33 +0530

    first file

---
+++ qr_code_generator.py
@@ -0,0 +1,32 @@
+import qrcode
+
+data=input("Enter the text or Url: ")
+filename=input("Enter file name: ")
+
+qr=qrcode.QRCode(box_size=10, border=4)
+qr.add_data(data)
+
+image=qr.make_image(fill_color='black',back_color='white')
+image.save(filename)
+
+print(f"File saved as {filename}")
+
+
+
+# import qrcode
+
+# data = input("Enter the text or URL: ")
+# filename = input("Enter file name (e.g., qr.png): ")
+
+# qr = qrcode.QRCode(
+#     box_size=10,
+#     border=4
+# )
+
+# qr.add_data(data)
+# qr.make(fit=True)
+
+# image = qr.make_image(fill_color="black", back_color="white")
+# image.save(filename)
+
+# print(f"File saved as {filename}")
